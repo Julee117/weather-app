d9a34bdc35dde5ceab42ad1b40e6f6b1337a0a4f
github username <juliannne@gmail.com>
2018-06-13 21:30:16 -0400

    Modify API url, import CurrentWeatherList

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
 import React, { Component } from 'react';
+import CurrentWeatherList from "./CurrentWeatherList";
 import './App.css';
 
-const APIURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+const APIURL = "https://api.openweathermap.org/data/2.5/group?id=5128581,5368361,4699066,4164138,5809844";
 const APIKEY = `&appid=${process.env.REACT_APP_API_KEY}`;
 const UNITS = "&units=imperial";
-const CITIES = ["new+york", "los+angeles", "houston", "miami", "seattle"]
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      cityWeather: []
+      cityWeather: {},
     }
   }
 
   componentDidMount() {
-    CITIES.map((city) => {
-      return fetch(`${APIURL}${city}${APIKEY}${UNITS}`)
-        .then(response => response.json())
-        .then(cityWeather => this.setState({cityWeather}))
-    })
+    fetch(`${APIURL}${APIKEY}${UNITS}`)
+      .then(response => response.json())
+      .then(cityWeather => this.setState({cityWeather: cityWeather.list}))
   }
 
   render() {
     const {cityWeather} = this.state
-    console.log("List of weathers", cityWeather)
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Weather App</h1>
         </header>
+        <CurrentWeatherList cityWeather={cityWeather}/>
       </div>
     );
   }
