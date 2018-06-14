8ec87162b25949ced54390db8018bcde22dbd8ac
github username <juliannne@gmail.com>
2018-06-14 12:30:30 -0400

    Modify alt

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -5,10 +5,9 @@ const CurrentWeather = ({weather}) =>
     <p>{weather.name}</p>
     <p>{Math.round(weather.main.temp)}&deg;</p>
     <p>
-      <img src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`} alt="Weather image"/><br />
+      <img src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`} alt="weather"/><br />
       {weather.weather[0].description}
     </p>
   </div>
-
 
 export default CurrentWeather;
