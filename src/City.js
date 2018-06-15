d43cc7220abb926395ed854793369d7ee853e029
github username <juliannne@gmail.com>
2018-06-15 10:45:17 -0400

    Add constructor, componentDidMount

--- src/City.js
+++ src/City.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 
-class City extends Component {
-  
-  render() {
-    const { match } = this.props;
+const URL = `https://api.openweathermap.org/data/2.5/forecast?q=`;
+const KEY = `us&appid=${process.env.REACT_APP_API_KEY}`;
+const UNITS = "&units=imperial";
 
+class City extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      climate: {},
+    }
+  }
+
+  componentDidMount() {
+    fetch(`${URL}${this.props.match.params.name},${KEY}${UNITS}`)
+      .then(response => response.json())
+      .then(climate => this.setState({climate: climate.list}))
+  }
+
+  render() {
     return(
-      <div>{match.params.name}</div>
+      <div>hi</div>
     )
   }
+
 }
 
 export default City;
