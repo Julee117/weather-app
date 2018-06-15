c29d6e6c6e87aafb5a1d63207330c8ae2c901c03
github username <juliannne@gmail.com>
2018-06-15 17:12:07 -0400

    Add height, width to image

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const CurrentWeather = ({weather}) =>
+
   <div className="box">
-    <Link key={weather.id} to={`/${weather.name}`}>
-    <p>{weather.name}</p>
+    <Link className="cityLink" key={weather.id} to={`/${weather.name}`}>
+    <p className="name">{weather.name}</p>
     <p>{Math.round(weather.main.temp)}&deg;</p>
     <p>
-      <img src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`} alt="weather"/><br />
+      <img src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`} alt="weather" height="70" width="70"/><br />
       {weather.weather[0].description}
     </p>
     </Link>
