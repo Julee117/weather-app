9c46399d593c39344a46f71c1d1f5f7a5dcb5040
github username <juliannne@gmail.com>
2018-06-15 08:02:56 -0400

    Add Link

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CurrentWeather = ({weather}) =>
   <div className="box">
+    <Link key={weather.id} to={`/${weather.name}`}>
     <p>{weather.name}</p>
     <p>{Math.round(weather.main.temp)}&deg;</p>
     <p>
       <img src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`} alt="weather"/><br />
       {weather.weather[0].description}
     </p>
+    </Link>
   </div>
 
 export default CurrentWeather;
