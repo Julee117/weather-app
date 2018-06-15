b2dc0710baf4e4e17089e0e002593192aeb589f2
github username <juliannne@gmail.com>
2018-06-15 01:55:27 -0400

    Add Switch and route

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import City from './City';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <Router>
-    <Route path="/" component={App}></Route>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route exact path={`/:name`} component={City} />
+    </Switch>
   </Router>,
   document.getElementById('root'));
 registerServiceWorker();
