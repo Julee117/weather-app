9ebac1942c1893da40ed765c2d3ee7dfbfa645a2
github username <juliannne@gmail.com>
2018-06-15 17:13:03 -0400

    Modify to class, create date, add height, width to image

--- src/CityForecast.js
+++ src/CityForecast.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const CityForecast = ({day}) =>
-  <div className="box">
-    <p>{Math.round(day.main.temp)}&deg;</p>
-    <p>
-      <img src={`http://openweathermap.org/img/w/${day.weather[0].icon}.png`} alt="weather"/><br />
-      {day.weather[0].description}
-    </p>
-  </div>
+class CityForecast extends Component {
+
+  render() {
+    const { day } = this.props;
+    let date = new Date(day.dt_txt).toString().split(" ");
+
+    return (
+      <div className="box">
+        <p>{date[0]}</p>
+        <p>{date[1]} {date[2]}</p>
+        <p>{Math.round(day.main.temp)}&deg;</p>
+        <p>
+          <img src={`http://openweathermap.org/img/w/${day.weather[0].icon}.png`} alt="weather" height="70" width="70"/><br />
+          {day.weather[0].description}
+        </p>
+      </div>
+    )
+  }
+}
 
 export default CityForecast;
